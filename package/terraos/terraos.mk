318589149514b4f1027f344df3c67aa2626db770
Toshit Chawda <r58playz@gmail.com>
2024-01-18 23:58:45 -0800

    update terra commit

--- package/terraos/terraos.mk
+++ package/terraos/terraos.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-TERRAOS_VERSION=1.0.1
-TERRAOS_SITE=$(call github,r58Playz,terraos,8925e19ed90e9d3a22663dbbf519cc33c3509f6f)
+TERRAOS_VERSION=2.0.0
+TERRAOS_SITE=$(call github,r58Playz,terraos,b1fe7030dfb6dc81521f762375cf9cba4250d288)
 TERRAOS_LICENSE=GPL-3.0
 TERRAOS_LICENSE_FILES=LICENSE
 
