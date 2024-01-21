d7d984fcfd73081f308f4d476fabe3011f0b6c8d
Toshit Chawda <r58playz@gmail.com>
2024-01-21 11:43:53 -0800

    fix arm

--- package/terraos/terraos.mk
+++ package/terraos/terraos.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TERRAOS_VERSION=2.0.0
-TERRAOS_SITE=$(call github,r58Playz,terraos,b1fe7030dfb6dc81521f762375cf9cba4250d288)
+TERRAOS_SITE=$(call github,r58Playz,terraos,72cea948667eb5d60e92c8e55263649de61d262c)
 TERRAOS_LICENSE=GPL-3.0
 TERRAOS_LICENSE_FILES=LICENSE
 
