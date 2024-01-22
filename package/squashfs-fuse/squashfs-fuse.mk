845a74ae9083d7477c0ed3cdc0ac8b029fe77176
Toshit Chawda <r58playz@gmail.com>
2024-01-21 17:13:07 -0800

    update squashfs-fuse and terra

--- package/squashfs-fuse/squashfs-fuse.mk
+++ package/squashfs-fuse/squashfs-fuse.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SQUASHFS_FUSE_VERSION = 1f980303b89c779eabfd0a0fdd36d6a7a311bf92
+SQUASHFS_FUSE_VERSION = 3f4dd2928ab362f8b20eab2be864d8e622472df5
 SQUASHFS_FUSE_SITE = $(call github,vasi,squashfuse,$(SQUASHFS_FUSE_VERSION))
 SQUASHFS_FUSE_DEPENDENCIES = libfuse host-pkgconf xz zstd
 SQUASHFS_FUSE_LICENSE = BSD-2-Clause
--- package/terraos/terraos.mk
+++ package/terraos/terraos.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TERRAOS_VERSION=2.0.0
-TERRAOS_SITE=$(call github,r58Playz,terraos,72cea948667eb5d60e92c8e55263649de61d262c)
+TERRAOS_SITE=$(call github,r58Playz,terraos,1e6e9d4d11b3589eef8f290f0d7cc327a57d5d48)
 TERRAOS_LICENSE=GPL-3.0
 TERRAOS_LICENSE_FILES=LICENSE
 
