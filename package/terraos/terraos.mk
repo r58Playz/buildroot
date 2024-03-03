e5d0444ac19dd18436f984e78d1d4be96b02f354
Toshit Chawda <r58playz@gmail.com>
2024-03-02 20:58:58 -0800

    update terraos

--- package/terraos/terraos.mk
+++ package/terraos/terraos.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TERRAOS_VERSION=2.0.0
-TERRAOS_SITE=$(call github,r58Playz,terraos,1e6e9d4d11b3589eef8f290f0d7cc327a57d5d48)
+TERRAOS_SITE=$(call github,r58Playz,terraos,c56fb1c07c3c16ad3ba09d7a7d1a7a12b6257ecc)
 TERRAOS_LICENSE=GPL-3.0
 TERRAOS_LICENSE_FILES=LICENSE
 
